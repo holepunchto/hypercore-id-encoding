f4606dc934aab605fe7c24599303f825d7b6439d
Andrew Osheroff <andrewosh@gmail.com>
2022-06-18 14:45:34 +0200

    Add README

---
+++ README.md
@@ -0,0 +1,44 @@
+# hypercore-id-encoder
+
+Encodes Hypercore keys into z-base32 ids, and decodes both hex/z-base32 strings.
+
+```
+npm install hypercore-id-encoder
+```
+
+## Usage
+
+```js
+const { encode, decode } = require('hypercore-id-encoder')
+
+const id = encode(core.key) // (z-base32 String)
+const hexEncoded = core.key.toString('hex')
+
+const core = new Hypercore(ram, decode(id)) 
+const core2 = new Hypercore(ram, decode(hexEncoded)) // Will also work with hex
+```
+
+## API
+
+#### `const id = encode(hypercoreKey)`
+
+Encodes a 32-byte Hypercore key into a z-base32 id.
+
+`hypercoreKey` must be a Buffer or an ArrayBuffer.
+
+#### `const buf = decode(hypercoreId)`
+
+Decodes an id into a Hypercore key.
+
+`hypercoreId` must be a String.
+
+If `hypercoreId` is a 52-character String, it will be decoded as z-base32.
+
+If `hypercoreId` is a 64-character String, it will be decoded as hex.
+
+## License
+
+MIT
+
+
+
---
+++ test.js
@@ -0,0 +1,44 @@
+const test = require('brittle')
+const Hypercore = require('hypercore')
+const ram = require('random-access-memory')
+const z32 = require('z32')
+const b = require('b4a')
+
+const { encode, decode } = require('.')
+
+test('encodes/decodes a hypercore key as z-base32', async t => {
+  const core = new Hypercore(ram)
+  await core.ready()
+  console.log('core.key:', core.key)
+  const core2 =  new Hypercore(ram, decode(encode(core.key)))
+  await core2.ready()
+  t.alike(core2.key, core.key)
+})
+
+test('decodes a hex-encoded hypercore key', async t => {
+  const core = new Hypercore(ram)
+  await core.ready()
+  t.alike(decode(encode(core.key)), decode(b.toString(core.key, 'hex')))
+})
+
+test('invalid keys', t => {
+  const keys = [
+    'hello world',
+    b.alloc(63)
+  ]
+  for (const key of keys) {
+    t.exception(() => encode(key))
+  }
+})
+
+test('invalid ids', t => {
+  const ids = [
+    b.alloc(64),
+    'hello world',
+    Array.from({ length: 52 }).fill('Z').join(''),
+    Array.from({ length: 64 }).fill('z').splice(0, 1, 'Z').join('')
+  ]
+  for (const id of ids) {
+    t.exception(() => decode(id))
+  }
+})
